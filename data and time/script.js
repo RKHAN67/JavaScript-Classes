afe3cd3554536f12fa211e9dbde76650f08f0683
Rizwan Khan <rizwan819101@gmail.com>
2026-01-19 12:47:17 +0500

    date and time example

---
+++ data and time/script.js
@@ -0,0 +1,20 @@
+const currentDate = new Date();
+const strDate = currentDate.toString();
+
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+let day = currentDate.getDay();
+let date = currentDate.getDate();
+let month = currentDate.getMonth();
+let year = currentDate.getFullYear();
+let hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+let seconds = currentDate.getSeconds();
+
+const formattedDate = `
+${days[day]} ${date} ${months[month]} ${year} ${hours}:${minutes}:${seconds}`;
+
+console.log(formattedDate);
+
+console.log(days[currentDate.getDay()], currentDate.getDate(), months[currentDate.getMonth()], currentDate.getFullYear(), currentDate.getHours(),':', currentDate.getMinutes(),':', currentDate.getSeconds());
